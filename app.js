7ebfec2a721d3dc48e4a76b4674bf20c1ec37d5b
subratkumar1804 <68953830+subratkumar1804@users.noreply.github.com>
2020-07-30 10:09:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function() { 
+app.listen(process.env.PORT || 3000, function() { 
   console.log('Server listening on port 3000'); 
 });
